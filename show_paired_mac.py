1e808a43aa46e1702377cdac18894578ef9dde77
Bunny Mickley <bunny@bunnynet.org>
2021-12-29 17:05:47 -0800

    Allow this to work with a D4 controller

--- show_paired_mac.py
+++ show_paired_mac.py
@@ -20,11 +20,11 @@ try:
     print("Opening the device")
 
     h = hid.device()
-    h.open(1356, 1476)  # Sony Dualshock 4
+    h.open(1356, 2508)  # Sony Dualshock 4
 
     print("Manufacturer: %s" % h.get_manufacturer_string())
     print("Product: %s" % h.get_product_string())
-    print("Serial No: %s" % h.get_serial_number_string())
+    # print("Serial No: %s" % h.get_serial_number_string())
 
     # enable non-blocking mode
     h.set_nonblocking(1)
@@ -37,12 +37,44 @@ try:
     for byte in remote_mac:
         print(format(byte, "02x"))
 
+    set_mac_report = [
+        0x13,
+        0xCE,
+        0xFA,
+        0xCE,
+        0xFA,
+        0x66,
+        0x06,
+        0x00,
+        0x11,
+        0x22,
+        0x33,
+        0x44,
+        0x55,
+        0x66,
+        0x77,
+        0x88,
+        0x99,
+        0xAA,
+        0xBB,
+        0xCC,
+        0xDD,
+        0xEE,
+        0xFF,
+    ]
+    #written = h.send_feature_report(set_mac_report)
+    #print(f"{written} bytes written")
+
     # write some data to the device
     # print("Write the data")
     # h.write([0, 63, 35, 35] + [0] * 61)
 
     # wait
-    # time.sleep(0.05)
+    #time.sleep(0.1)
+
+    #print("0x12 --\n\n")
+    #feature_report = h.get_feature_report(0x12, 255)
+    #print(feature_report)
 
     # read back the answer
     # print("Read the data")
